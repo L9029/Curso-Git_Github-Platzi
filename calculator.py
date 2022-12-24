5f4e637e713e7e97b18c257de1f98d9f425c6eb6
l9029 <l9029@kali.l9029>
2022-12-23 22:45:47 -0400

    Calculadora V1.1 "Se creo el menu de inicio y la despedida del programa"

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import time
+
 def plus(num1, num2):
     result = num1 + num2
     print(f"La suma de {num1} + {num2} es: {result}")
@@ -13,3 +15,33 @@ def times(num1, num2):
 def divide(num1, num2):
     result = num1 / num2
     print(f"La división de {num1} / {num2} es: {result}")
+
+menu = input("""
+            ...Bienvenido...
+                
+        ¿Qué operación desea realizar?
+            
+        1)Suma
+        2)Resta
+        3)Multiplicación
+        4)División
+        
+        Escribe -exit- para salir
+        
+        Seleción: """)
+
+if menu == 1:
+    pass
+
+elif menu == 2:
+    pass
+
+elif menu == 3:
+    pass
+
+elif menu == 4:
+    pass
+
+elif menu == "exit":
+    print("\n...Saliendo...\n")
+    time.sleep(5)
