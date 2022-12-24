2f1d1313eae4884d1c9cd3c9027924178330020c
l9029 <l9029@kali.l9029>
2022-12-23 22:10:18 -0400

    Calculadora V1.0 "Se crearón las funciones de las operaciones simples"

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def plus(num1, num2):
+    result = num1 + num2
+    print(f"La suma de {num1} + {num2} es: {result}")
+
+def less(num1, num2):
+    result = num1 - num2
+    print(f"La resta de {num1} - {num2} es: {result}")
+
+def times(num1, num2):
+    result = num1 * num2
+    print(f"La multiplicación de {num1} * {num2} es: {result}")
+
+def divide(num1, num2):
+    result = num1 / num2
+    print(f"La división de {num1} / {num2} es: {result}")
